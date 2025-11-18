5af0b0596e095ab41c5caddaeaca90dd711c68a5
abhibhrt <adjack720@gmail.com>
2025-11-18 23:52:10 +0530

    fixed jwt secret

--- server/utils/jwt.util.js
+++ server/utils/jwt.util.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken';
 
 export const assignJwt = (payload) => {
-    return jwt.sign(payload, process.env.jwt_secret, {
+    return jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: '7d'
     });
 };
 
 export const verifyJwt = (token) => {
     try {
-        return jwt.verify(token, process.env.jwt_secret);
+        return jwt.verify(token, process.env.JWT_SECRET);
     } catch {
         return null;
     }
