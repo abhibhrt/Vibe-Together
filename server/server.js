b9c3108ad9286f59c9489b9f544629ed969f9c7c
abhibhrt <adjack720@gmail.com>
2025-11-18 14:57:50 +0530

    allowed client

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://vibe-together.vercel.app",
         credentials: true
     })
 );
