62574de37e689568ec467063aaa39c890f27af4c
Abhishek Bharti <148630518+abhibhrt@users.noreply.github.com>
2025-11-18 16:22:21 +0530

    Refactor CORS middleware configuration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// Packages Imported
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
@@ -6,37 +5,26 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
-// Modules Imported
 import { connectDb } from './config/db.config.js';
 
-// Routes Imported
 import AuthRoutes from './routes/user.route.js';
 import CoupleRoute from './routes/couple.route.js';
 import PlaylistRoute from './routes/playlist.route.js';
 import MusicRoute from './routes/music.route.js';
 
-// Modules Applied
 dotenv.config();
 const app = express();
 connectDb();
 
-// Middlewares
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-/*app.use(
-    cors({
-        origin: '*',
-        credentials: true
-    })
-); */
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      callback(null, true);
-    },
+    origin: true,  
     credentials: true,
     methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
     allowedHeaders: "*"
@@ -49,11 +37,9 @@ app.use('/api/couple', CoupleRoute);
 app.use('/api/playlist', PlaylistRoute);
 app.use('/api/music', MusicRoute);
 
-// Health Check
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'api running..' });
+  res.status(200).json({ message: 'api running..' });
 });
 
-// Server Listen
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`api running port: ${port}`));
